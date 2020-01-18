79d18b0eef809c2023bd10ca34f5a62450fd3075
shitalluitel <ctalluitel@gmail.com>
2020-01-18 18:30:05 +0545

    change wsgi setting

--- LibraryManagementSystem/wsgi.py
+++ LibraryManagementSystem/wsgi.py
@@ -11,10 +11,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
-from my_project import MyWSGIApp
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "LibraryManagementSystem.settings")
 
-application = MyWSGIApp()
+application = get_wsgi_application()
 application = WhiteNoise(application, root='staticfiles')
 # application.add_files()
